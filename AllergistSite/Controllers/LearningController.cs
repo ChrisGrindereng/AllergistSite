fc2e66729fefe4d98ccc32eeb6ae4506f6d19e40
cog042 <cog042@gmail.com>
2017-03-28 00:37:02 -0500

    added dropdown nav and embeded videos

---
+++ AllergistSite/Controllers/LearningController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AllergistSite.Controllers
+{
+    public class LearningController : Controller
+    {
+        // GET: Learning
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EpiPen()
+        {
+            return View();
+        }
+        
+        public ActionResult AVQ()
+        {
+            return View();
+        }
+
+        public ActionResult Inhaler()
+        {
+            return View();
+        }
+
+        public ActionResult Spacer()
+        {
+            return View();
+        }
+    }
+}
